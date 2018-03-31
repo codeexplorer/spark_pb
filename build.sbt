de0607de6eb118736c269b64e8f497f95543151b
Sandeep Bajaj <sandeep.bajaj@hpe.com>
2018-03-31 09:55:32 -0700

    Adding frameless injections to get around enums

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+
 name := "sparkpb"
 version := "0.0.0-SNAPSHOT"
 organization := "com.codeexplorer.sparkpb"
@@ -10,7 +11,6 @@ val excludeScalaLang = ExclusionRule(organization = "org.scala-lang")
 val excludeNettyIo = ExclusionRule(organization = "org.jboss.netty")
 val excludeSparkProject = ExclusionRule(organization = "org.spark-project.spark")
 
-//resolvers += "Mr.Powers" at "https://dl.bintray.com/spark-packages/maven/"
 dependencyOverrides += "org.scala-lang" % "scala-compiler" % scalaVersion.value
 
 libraryDependencies ++= Seq(
@@ -28,10 +28,11 @@ libraryDependencies ++= Seq(
     ExclusionRule(organization = "org.apache.hadoop")
   ),
   "com.thesamet.scalapb" %% "scalapb-runtime" % scalaPbVersion,
-  "com.thesamet.scalapb" %% "sparksql-scalapb" % "0.7.0"
+  "com.thesamet.scalapb" %% "sparksql-scalapb" % "0.7.0",
+  "org.typelevel" %% "frameless-dataset" % "0.5.0"
 )
+
 PB.protoSources.in(Compile) := Seq(sourceDirectory.in(Compile).value / "protobufs")
 PB.targets in Compile := Seq(scalapb.gen() -> (sourceManaged in Compile).value,
   new scalapb.UdtGenerator(flatPackage = false) -> (sourceManaged in Compile).value)
-
 
--- src/main/scala/com/codeexplorer/sparkpb/sparkpb.scala
+++ src/main/scala/com/codeexplorer/sparkpb/sparkpb.scala
@@ -1,14 +1,19 @@
 package com.codeexplorer.sparkpb
 
 import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.types.StructType
 import com.codeexplorer.sparkpb.sample._
+import java.io.{ByteArrayOutputStream, ObjectOutputStream}
+import org.apache.spark.sql.functions._
+import frameless._
 
 object sparkPB {
+
   def main(args: Array[String]): Unit = {
 
     def appName: String = "sparkPB"
 
-    val spark = SparkSession.builder
+    implicit val spark = SparkSession.builder
       .appName(appName)
       .enableHiveSupport()
       .getOrCreate()
@@ -17,6 +22,7 @@ object sparkPB {
 
     SampleProtoUdt.register()
 
+    //Construct metric from constructors
     val submetric = SubMetric().update(
       _.smid := 1,
       _.violationType := SubMetric.violation_type_t.NON_COMPLIANT,
@@ -25,10 +31,53 @@ object sparkPB {
       _.timestamp := 12345,
       _.sessionCnt := 1
     )
-    val metric = Seq(Metric("m1", Seq(submetric)))
 
-    val metricDF = spark.sqlContext.createDataset(metric)
-    metricDF.show
+    implicit val violationToInt: Injection[SubMetric.violation_type_t, Int] =
+      new Injection[SubMetric.violation_type_t, Int] {
+        def apply(d: SubMetric.violation_type_t): Int = d match {
+          case SubMetric.violation_type_t.NON_COMPLIANT => 0
+          case SubMetric.violation_type_t.COMPLIANT => 1
+        }
+
+        def invert(l: Int): SubMetric.violation_type_t = l match {
+          case 0 => SubMetric.violation_type_t.NON_COMPLIANT
+          case 1 => SubMetric.violation_type_t.COMPLIANT
+        }
+      }
+
+    implicit val reasonToInt: Injection[SubMetric.metric_reason_t, Int] =
+      new Injection[SubMetric.metric_reason_t, Int] {
+        def apply(d: SubMetric.metric_reason_t): Int = d match {
+          case SubMetric.metric_reason_t.REASON_1 => 1
+          case SubMetric.metric_reason_t.REASON_2 => 2
+          case SubMetric.metric_reason_t.REASON_3 => 3
+          case SubMetric.metric_reason_t.REASON_4 => 4
+        }
+
+        def invert(l: Int): SubMetric.metric_reason_t = l match {
+          case 1 => SubMetric.metric_reason_t.REASON_1
+          case 2 => SubMetric.metric_reason_t.REASON_2
+          case 3 => SubMetric.metric_reason_t.REASON_3
+          case 4 => SubMetric.metric_reason_t.REASON_4
+        }
+      }
+
+    val metric: Metric = Metric("m1", Seq(submetric))
+
+    // Direct creation of Datasets with enums fails, uncomment below to see
+    //val metricDFdirect = spark.sqlContext.createDataset(Seq(metric))
+    //metricDFdirect.show
+
+    // But typed dataset passes
+    val metricTDS = TypedDataset.create(Seq(metric))
+    metricTDS.toDF.show
+
+    // Try to Construct metric from byteArray
+    val metricBytes = metric.toByteArray
+
+    //How to make the following work with injection
+    val byteDF = spark.sparkContext.parallelize(Seq(metricBytes)).toDF.as[Array[Byte]]
+    val byteMetric = byteDF.map(row => Metric.parseFrom(row))
+    byteMetric.show(false)
   }
-
 }
